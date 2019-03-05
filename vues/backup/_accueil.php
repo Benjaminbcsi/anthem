5ce12e6ce3f112d810e2a9a9802ce789ee894c62
DEVOPS2 <DEVOPS2@ecole-esgc.fr>
2019-03-05 08:42:03 +0100

    delete backup

--- vues/backup/_accueil.php
+++
@@ -1,9 +0,0 @@
-<div class="nopb" id="accueil">
-	<div id="supportContenuAccueil">
-		<h1 id="titreAccueil">Créez et rejoignez des équipes <br/> pour jouer en ligne</h1>
-		<div id="supportBoutons">
-			<button id="boutonConnexion" onclick="javascript:navigate('<?php echo encrypt('connexion','DISII28');?>');">Connexion</button>
-			<button id="boutonInscription" onclick="javascript:navigate('<?php echo encrypt('inscription','DISII28');?>');">Inscription</button>
-		</div>
-	</div>
-</div>
